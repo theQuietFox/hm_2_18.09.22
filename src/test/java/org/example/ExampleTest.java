2cd21571b510913763c4ddc07d5a168fdca91ef7
theQuietFox <irina.korehova@gmail.com>
2022-09-19 00:13:58 +0300

    fix logic

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -7,11 +7,11 @@ public class ExampleTest {
 
 @Test
     void exampleTest0() {
-    Assertions.assertTrue(2 > 1);
+    Assertions.assertTrue(2 >= 1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(3 >= 2);
     }
 }
